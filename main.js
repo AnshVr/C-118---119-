60e2f2463077f43f2c0b1958e0834c61e7805d80
AnshVr <85823104+AnshVr@users.noreply.github.com>
2022-06-09 22:45:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -33,11 +33,11 @@ function setup() {
 
 
 function draw() {
-  // Set stroke weight to 10
+  
   strokeWeight(13);
-  // Set stroke color to black
+  
   stroke(0);
-  // If mouse is pressed, draw line between previous and current mouse positions
+ 
   if (mouseIsPressed) {
     line(pmouseX, pmouseY, mouseX, mouseY);
   }
